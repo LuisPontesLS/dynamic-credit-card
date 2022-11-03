958149dd8c42e5856e8b08b6019694fb980e9abd
LuisPontesLS <leospontes@hotmail.com>
2022-11-03 17:21:36 -0300

    Capturar nome digitado

--- src/main.js
+++ src/main.js
@@ -67,10 +67,25 @@ const cardNumberPatter = {
     const foundMask = dynamicMasked.compiledMasks.find(function (item) {
       return number.match(item.regex)
     })
-
-    console.log(foundMask)
-
+    // console.log(foundMask)
     return foundMask
   },
 }
 const cardNumberMasked = IMask(cardNumber, cardNumberPatter)
+
+const addButton = document.querySelector("#add-card")
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado!")
+})
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault()
+})
+
+const cardHolder = document.querySelector("#card-holder")
+cardHolder.addEventListener("input", () => {
+  const ccHolder = document.querySelector(".cc-holder .value")
+
+  ccHolder.innerText =
+    cardHolder.value.length === 0 ? "FULANO DA SILVA" : cardHolder.value
+})
